e2d762b8448edbc30998ad5b792c2fb6af46a21a
graemerocher <graeme.rocher@gmail.com>
2012-01-19 17:08:23 +0100

    changed to spock method interceptor for @WebReport but still not working, probably an old version of spock to blame

--- src/test/groovy/grails/functional/tests/internal/WebReportExtension.groovy
+++ src/test/groovy/grails/functional/tests/internal/WebReportExtension.groovy
@@ -6,6 +6,8 @@ import org.spockframework.runtime.model.SpecInfo
 import org.spockframework.runtime.AbstractRunListener
 import org.spockframework.runtime.model.ErrorInfo
 import com.sun.xml.internal.bind.v2.TODO
+import org.spockframework.runtime.extension.AbstractMethodInterceptor
+import org.spockframework.runtime.extension.IMethodInvocation
 
 /**
  * The web report extension guts
@@ -20,22 +22,80 @@ class WebReportExtension extends AbstractAnnotationDrivenExtension<WebReport>{
 
     @Override
     void visitSpec(SpecInfo spec) {
-        if(specEnabled)
-            spec.addListener(new FailureReportListener(spec))
+        if(specEnabled) {
+            
+           spec.addInterceptor(new FailureReportListener(spec))
+        }
     }
 
-    class FailureReportListener extends AbstractRunListener {
-        SpecInfo spec
+}
+class FailureReportListener extends AbstractMethodInterceptor {
+    SpecInfo spec
 
-        FailureReportListener(SpecInfo spec) {
-            this.spec = spec
+    FailureReportListener(SpecInfo spec) {
+        this.spec = spec
+    }
+
+    @Override
+    void interceptSetupMethod(IMethodInvocation invocation) {
+        invocation.proceed()
+    }
+
+    @Override
+    void interceptCleanupMethod(IMethodInvocation invocation) {
+        invocation.proceed()
+    }
+
+    @Override
+    void interceptSetupSpecMethod(IMethodInvocation invocation) {
+        invocation.proceed()
+    }
+
+    @Override
+    void interceptCleanupSpecMethod(IMethodInvocation invocation) {
+        invocation.proceed()
+    }
+
+    @Override
+    void interceptFeatureMethod(IMethodInvocation invocation) {
+        println "FEATURE METHOD!"
+        invocation.proceed()
+    }
+
+    @Override
+    void interceptDataProviderMethod(IMethodInvocation invocation) {
+        invocation.proceed()
+    }
+
+    @Override
+    void interceptDataProcessorMethod(IMethodInvocation invocation) {
+        invocation.proceed()
+    }
+
+
+    @Override
+    void interceptSpecExecution(IMethodInvocation invocation) {
+        invocation.proceed()
+    }
+
+    @Override
+    void interceptFeatureExecution(IMethodInvocation invocation) {
+        try {
+            invocation.proceed()
+        } catch (e) {
+            try {
+                def content = invocation.target?.browser?.page?.downloadContent()
+                println "########################################################################################################"
+                println "                              Web Content"
+                println "########################################################################################################"
+                println content
+                println "########################################################################################################"
+            } catch (e2) {
+                // ignore
+            }
+            throw e
         }
-
-        @Override
-        void error(ErrorInfo error) {
-            // TODO: figure out how to dump the error from the web response here, problematic because we need a reference to the sepc instance
-        }
-
 
     }
+
 }
