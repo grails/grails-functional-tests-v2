fe318451d8ff49c4ef15dfec3ccdf159c842af09
graemerocher <graeme.rocher@gmail.com>
2012-01-20 15:13:16 +0100

    ported default codec test case to spock

--- src/test/groovy/grails/functional/tests/web/DefaultCodecSpec.groovy
+++ src/test/groovy/grails/functional/tests/web/DefaultCodecSpec.groovy
@@ -1,11 +1,22 @@
 package grails.functional.tests.web
 
+import grails.functional.tests.BaseApplicationSpec
+
 /**
- * Created by IntelliJ IDEA.
- * User: graemerocher
- * Date: 1/20/12
- * Time: 2:34 PM
- * To change this template use File | Settings | File Templates.
+ *
  */
-class DefaultCodecSpec {
+class DefaultCodecSpec extends BaseApplicationSpec{
+
+    // GRAILS-8405
+    void "Test resource tag with default codec enabled"() {
+        when:"A page is requested that using the request tag"
+            go 'demo'
+        then:"The resource tag renders correctly"
+            contains 'Image: <img src="/html-default-codec/static/images/grails_logo.png" alt="Grails"/>'
+    }
+
+    @Override
+    String getApplication() {
+        "html-default-codec"
+    }
 }
